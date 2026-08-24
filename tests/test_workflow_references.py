680083897553061a7d043f7a2153359f5510a4d2
ekolvah <ekolvah@gmail.com>
2026-08-24 12:33:19 +0300

    test: mark published workflow SHA as non-secret

--- tests/test_workflow_references.py
+++ tests/test_workflow_references.py
@@ -9,7 +9,7 @@ import yaml
 
 ROOT = Path(__file__).resolve().parents[1]
 _PIN = re.compile(r"^[^@]+@[0-9a-f]{40}$")
-_OWNER_REQUESTED_CARRIER_REVISION = "d3f6d238891dbaee6af10b18609e7e07f8901921"
+_OWNER_REQUESTED_CARRIER_REVISION = "d3f6d238891dbaee6af10b18609e7e07f8901921"  # pragma: allowlist secret
 
 
 def test_copier_workflow_references_share_a_published_revision() -> None:
